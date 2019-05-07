871d4d422f3a06711ed2f2ff8757c96a5dd0b627
Carlos Ponce <jcponce510@gmail.com>
2019-05-07 15:35:50 -0700

    Add fix to add new gif function and margin to form.

--- app.js
+++ app.js
@@ -1,103 +1,115 @@
 $(document).ready(function() {
 
   var topics = ["wolf", "killer whale", "star wars", "transformers", "g.i. joe","game of thrones", "sopranos", "dancing", "running", "coffee", "michael jordan"];
-  
 
   // Create buttons for each item in topics array.
-  for (var i = 0; i < topics.length; i++) {
-    var topicButton = $("<button type=\"button\" class=\"btn btn-info\"></button>");
-    topicButton.text(topics[i]);
-    $("#buttonsDiv").append(topicButton);
+  var makeGifBtns = function() {
+    for (var i = 0; i < topics.length; i++) {
+      var topicButton = $("<button type=\"button\" class=\"btn btn-info\"></button>");
+      topicButton.text(topics[i]);
+      $("#buttonsDiv").append(topicButton);
+    };
   };
+
+  makeGifBtns();
 
   /* When the user clicks on a gif button, the page should grab 10 static,
   non-animated gif images from the GIPHY API and place them on the page. */
-  $(".btn-info").on("click", function () {
+  var gogoGadgetGifs = function () {
+    $(".btn-info").on("click", function () {
 
-    // Clear out gifs from previously clicked button.
-    $("#gifsDiv").empty();
-
-    // q parameter for queryURL.
-    var searchQ = $(this).text();
-
-    // API key for queryURL.
-    var apiKey = "yNw4OuU5LCxWWiRST7F7qPW1qolhQxDc";
-
-    // Query URL for API call, need to enter value for parameter q.
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + searchQ + "&api_key=" + apiKey + "&limit=10";
-
-    // API call.
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    }).then(function(response) {
-
-      // Store the ten gifs requested from the API.
-      var results = response.data;
-      // var imageUrl;
-      
-      // Loop over each result item.
-      for (var i = 0; i < results.length; i++) {
-        
-        // Create div for gif.
-        var gifDiv = $("<div class=\"aGif\">");
-
-        // Store result item rating.
-        var rating = results[i].rating;
-
-        // Create p tag for rating.
-        var p = $("<p>").text("Rating: " + rating);
-        
-        // Create and store img tag.
-        var topicImage = $("<img class=\"img-fluid rounded\">");
-
-        // Set "state" data attribute.
-        topicImage.attr("data-state", "still");
-
-        // Set "still" data attribute.
-        topicImage.attr("data-still", results[i].images.fixed_height_still.url);
-        
-        // Set "animate" data attribute.
-        topicImage.attr("data-animate", results[i].images.fixed_height.url);
-        
-        // Set topicImage src attribute to imageUrl.
-        topicImage.attr("src", results[i].images.fixed_height_still.url);
-        
-        topicImage.attr("alt", searchQ + " image");
-
-        // Add gif and rating to gifDiv.
-        gifDiv.append(topicImage);
-        gifDiv.append(p);
-
-        // Append gifDiv to gifsDiv in HTML.
-        $("#gifsDiv").append(gifDiv);
-      };
-
-      // When user clicks one of the still GIPHY images, gif should animate.  If user clicks the gif again, it should stop playing.
-      $("img").on("click", function() {
-        var state = $(this).attr("data-state");
-        if (state === "still") {
-          $(this).attr("src", $(this).attr("data-animate"));
-          $(this).attr("data-state", "animate");
-        } else {
-          $(this).attr("src", $(this).attr("data-still"));
-          $(this).attr("data-state", $(this).attr("still"));
-        };
-      });
-
-    });
-  });
+      // Clear out gifs from previously clicked button.
+      $("#gifsDiv").empty();
   
+      // q parameter for queryURL.
+      var searchQ = $(this).text();
+  
+      // API key for queryURL.
+      var apiKey = "yNw4OuU5LCxWWiRST7F7qPW1qolhQxDc";
+  
+      // Query URL for API call, need to enter value for parameter q.
+      var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + searchQ + "&api_key=" + apiKey + "&limit=10";
+  
+      // API call.
+      $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).then(function(response) {
+  
+        // Store the ten gifs requested from the API.
+        var results = response.data;
+        // var imageUrl;
+        
+        // Loop over each result item.
+        for (var i = 0; i < results.length; i++) {
+          
+          // Create div for gif.
+          var gifDiv = $("<div class=\"aGif\">");
+  
+          // Store result item rating.
+          var rating = results[i].rating;
+  
+          // Create p tag for rating.
+          var p = $("<p>").text("Rating: " + rating);
+          
+          // Create and store img tag.
+          var topicImage = $("<img class=\"img-fluid rounded\">");
+  
+          // Set "state" data attribute.
+          topicImage.attr("data-state", "still");
+  
+          // Set "still" data attribute.
+          topicImage.attr("data-still", results[i].images.fixed_height_still.url);
+          
+          // Set "animate" data attribute.
+          topicImage.attr("data-animate", results[i].images.fixed_height.url);
+          
+          // Set topicImage src attribute to imageUrl.
+          topicImage.attr("src", results[i].images.fixed_height_still.url);
+          
+          topicImage.attr("alt", searchQ + " image");
+  
+          // Add gif and rating to gifDiv.
+          gifDiv.append(topicImage);
+          gifDiv.append(p);
+  
+          // Append gifDiv to gifsDiv in HTML.
+          $("#gifsDiv").append(gifDiv);
+        };
+  
+        // When user clicks one of the still GIPHY images, gif should animate.  If user clicks the gif again, it should stop playing.
+        $("img").on("click", function() {
+          var state = $(this).attr("data-state");
+          if (state === "still") {
+            $(this).attr("src", $(this).attr("data-animate"));
+            $(this).attr("data-state", "animate");
+          } else {
+            $(this).attr("src", $(this).attr("data-still"));
+            $(this).attr("data-state", $(this).attr("still"));
+          };
+        });
+  
+      });
+    });
+  };
+  
+  gogoGadgetGifs();
+
   /* When user clicks "Submit" button, the value from user input box is added into topics array. Then a function call that takes each topic in the array
    remakes the buttons on the page. */
   $(".btn-primary").on("click", function() {
+    // Clear out previously created buttons.
+    $("#buttonsDiv").empty();
+    
     // Grab user input for new gif and add it to "topics" array.
     var newGif = $("#addNewGif").val();
-    topics.append(newGif);
+    topics.push(newGif);
 
     // Call function to remake page to include newly added gif.
-    
+    makeGifBtns();
 
+    // Call function to restart click listener for gif buttons.
+    gogoGadgetGifs();
   });
 });
 
